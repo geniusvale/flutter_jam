1f6f95cca9185212d592dd2218eea0622c93e85c
geniusvale <ariiqromero@gmail.com>
2022-03-10 19:34:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_jam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_jam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FlutterJam());
+}
+
+class FlutterJam extends StatefulWidget {
+  const FlutterJam({Key? key}) : super(key: key);
+
+  @override
+  State<FlutterJam> createState() => _FlutterJamState();
+}
+
+class _FlutterJamState extends State<FlutterJam> {
+  var Jam = '';
+
+  void mulaiJam() {
+    Timer.periodic(Duration(seconds: 1), (_) {
+      var tgl = DateTime.now();
+      var formatJam = DateFormat.Hms().format(tgl);
+      setState(() {
+        Jam = formatJam;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    mulaiJam();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Jam',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Jam'),
+        ),
+        body: Center(child: Text(Jam, style: TextStyle(fontSize: 48),)),
+      ),
+    );
+  }
+}
